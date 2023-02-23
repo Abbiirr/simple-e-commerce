9431f599006c08206436b2ba9bdc904f394bbe17
Abir <abir.hossain2107@gmail.com>
2023-02-23 19:10:58 +0600

    Product screen using server

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
   const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      console.log(data);
+
+      setProduct(data);
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <>
