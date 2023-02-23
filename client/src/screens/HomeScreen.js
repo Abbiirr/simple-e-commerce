f73320594adcb65592b38d3ffd0cd80a1bdc91fc
Abir <abir.hossain2107@gmail.com>
2023-02-23 19:10:17 +0600

    Home screen now using server

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -9,7 +9,8 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get("http://localhost:5000/api/products");
+      const { data } = await axios.get("/api/products");
+      console.log(data);
 
       setProducts(data);
     };
