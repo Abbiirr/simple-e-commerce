b10498066b5b76091b78a451628031ab5a8818f6
Abir <abir.hossain2107@gmail.com>
2023-02-23 19:41:32 +0600

    Using .env now

--- server/App.js
+++ server/App.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const products = require("./data/products");
+const dotenv = require("dotenv");
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 
@@ -16,4 +20,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.SERVER_PORT;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
